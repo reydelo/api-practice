19171f3131cb7a2b630317ae354884aed5d40fe1
Reyna DeLoge <reyna.deloge@gmail.com>
2015-04-22 15:32:38 -0600

    ajax call from localhot api

---
+++ main.js
@@ -0,0 +1,16 @@
+$(function(){
+  var source = $("#api-template").html();
+  var template = Handlebars.compile(source);
+
+
+  $.ajax({
+    url: "http://localhost:3000/api/products.json"
+  }).done(function(data) {
+    var theProducts = data;
+
+    var html = template(theProducts);
+    $('body').append(html);
+  });
+
+
+});
